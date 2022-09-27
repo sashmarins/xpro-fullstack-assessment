4c39f529fcbd8952e5a24152dfda1d97c4497d75
sashmarins <sasha.marinskiiy@gmail.com>
2022-09-27 10:48:18 -0400

    users created

--- aalbackend/aalbackend/settings.py
+++ aalbackend/aalbackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'aalbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Sasha_Marinskiy',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
     }
 }
 
